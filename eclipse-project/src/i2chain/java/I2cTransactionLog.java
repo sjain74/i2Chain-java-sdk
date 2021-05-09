c487b7eb4917605b1c7f0a0f8eb94095ee74237e
Sanjay Jain <sjain74@gmail.com>
2021-05-09 13:40:13 -0700

    fixing tabs to spaces issue

--- eclipse-project/src/i2chain/java/I2cTransactionLog.java
+++ eclipse-project/src/i2chain/java/I2cTransactionLog.java
@@ -1,22 +1,22 @@
 package i2chain.java;
 
 public class I2cTransactionLog {
-	
-	// The action performed on the document, e.g., Revoked, Shared, Chained, etc.
-	String 	action;
-	
-	// The user who performed the action
-	String 	by;
+    
+    // The action performed on the document, e.g., Revoked, Shared, Chained, etc.
+    String     action;
+    
+    // The user who performed the action
+    String     by;
 
-	// Document classification, e.g., Restricted, Confidential, Secret, Top Secret etc.
-	String	classification;
+    // Document classification, e.g., Restricted, Confidential, Secret, Top Secret etc.
+    String    classification;
 
-	// The recipient
-	String	recipient;
+    // The recipient
+    String    recipient;
 
-	// Status, e.g., Successful, Failed, Warning, etc.
-	String	status;
+    // Status, e.g., Successful, Failed, Warning, etc.
+    String    status;
 
-	// Transaction date and time, e.g. 02-08-2021, 16:14; TBD: exact format to be described 
-	String 	time;
+    // Transaction date and time, e.g. 02-08-2021, 16:14; TBD: exact format to be described 
+    String     time;
 }
